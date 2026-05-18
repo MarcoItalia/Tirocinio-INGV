765d61df4eb05435e2f2a8f0a743ac8f5c5ae0cf
MarcoItalia <italia.marco.sr@gmail.com>
2026-05-18 16:35:32 +0200

    feat(blackbox): fixed behaviour to match that of the actual "blackbox server"

--- windows/blackbox_test/blackbox_sim.py
+++ windows/blackbox_test/blackbox_sim.py
@@ -1,19 +1,30 @@
 import zmq
 
 context = zmq.Context()
-socket = context.socket(zmq.REP)
+socket = context.socket(zmq.ROUTER)  # zmq.REP
+context.setsockopt(zmq.ROUTER_MANDATORY, True)
+
 socket.bind("tcp://0.0.0.0:5000")
 
 try:
-    message = socket.recv_string()
+    # message = socket.recv()
+    # print(f"Received {message}")
+
+    message = socket.recv_multipart()
     print(f"Received {message}")
 
-    print("Sending 'msg1'")
+    socket.send_multipart([message[0], b"msg1"])
+    socket.send_multipart([message[0], b"msg2"])
+    socket.send_multipart([message[0], b"msg3"])
+    # socket.send(b"msg2")
+    # socket.send(b"msg3")
+
+    # print("Sending 'msg1'")
     # socket.send_string("msg1")
-    print("Sending 'msg2'")
+    # print("Sending 'msg2'")
     # socket.send_string("msg2")
-    print("Sending 'msg3'")
-    socket.send_multipart([b"msg1", b"msg2", b"msg3"])
+    # print("Sending 'msg3'")
+    # socket.send_multipart([b"msg1", b"msg2", b"msg3"])
 finally:
     socket.close()
     context.term()
