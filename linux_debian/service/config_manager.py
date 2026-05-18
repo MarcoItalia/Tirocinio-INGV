c35b51c09363dad2e0d13e52c22f93a3271c72e9
MarcoItalia <italia.marco.sr@gmail.com>
2026-05-18 12:55:42 +0200

    feat(service): implementation of multiple response and logic for the blackbox

--- linux_debian/service/config_manager.py
+++ linux_debian/service/config_manager.py
@@ -6,15 +6,25 @@ import dict_to_namespace as dtns
 
 def yaml_read(path_yaml: str = "config.yaml") -> SimpleNamespace:
     """Legge un file yaml e ritorna un oggetto facilmente leggibile da python"""
-    BASE_DIR = os.path.dirname(os.path.abspath(
+    base_dir = os.path.dirname(os.path.abspath(
         __file__))  # si protrebbe trasformare in una funzione
-    config_path = os.path.join(BASE_DIR, path_yaml)
+    config_path = os.path.join(base_dir, path_yaml)
 
     with open(config_path, encoding="utf-8") as f:
         return dtns.dict_to_namespace(yaml.safe_load(f))
 
 
-def str_constructor(config_namespace: SimpleNamespace = yaml_read()) -> SimpleNamespace:
-    """"""
-    return (str(config_namespace.socket.protocol) + "://" + str(config_namespace.socket.ip) +
-            ":" + str(config_namespace.socket.port))
+def str_constructor(config_namespace: SimpleNamespace = yaml_read(), whois: str = "client") -> str:
+    """Costruisce una stringa'protocollo://ip:porta' leggendo da un SimpleNS"""
+    if whois == "client":
+        return (str(config_namespace.socket.client.protocol) + "://" +
+                str(config_namespace.socket.client.ip) + ":" + str(config_namespace.socket.client.port))
+    elif whois == "server":
+        return (str(config_namespace.socket.server.protocol) +
+                "://" + str(config_namespace.socket.server.ip) + ":" + str(config_namespace.socket.server.port))
+    else:
+        try:
+            return (str(config_namespace.socket.protocol) + "://" +
+                    str(config_namespace.socket.ip) + ":" + str(config_namespace.socket.port))
+        except:
+            return -1
--- linux_debian/service/server_test.py
+++ linux_debian/service/server_test.py
@@ -1,18 +1,41 @@
 import zmq
 import config_manager
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
+context_client = zmq.Context()
+socket_client = context_client.socket(zmq.REP)
 config = config_manager.yaml_read("config.yaml")
+# non aspetta messaggi in sospeso alla chiusura
+# socket_client.setsockopt(zmq.LINGER, 0)
+
+
+context_blackbox = zmq.Context()
+socket_blackbox = context_blackbox.socket(
+    zmq.REQ)  # mettere nelle opzioni il multiple
 
 # bind(tcp://0.0.0.0:5555) connect(172.17.64.1)
-socket.bind(config_manager.str_constructor(config))
+socket_client.bind(config_manager.str_constructor(config))
+print("connected to client")
+socket_blackbox.connect(config_manager.str_constructor(config, "server"))
+print("connected to blackbox")
+try:
+    print("listening")
 
-message = socket.recv_string()
-print(f"Received {message}")
+    message = socket_client.recv_string()
+    print(f"Received {message}, now forwarding to blackbox")
 
-print("Sending 'Hello'")
-socket.send_string("Hello")
+    socket_blackbox.send_string(message)
+    print("listening to msg")
 
-socket.close()
-context.term()
+    message = socket_blackbox.recv_multipart()
+
+    print(f"Recived {message[0]}")
+    print(f"Recived {message[1]}")
+    print(f"Recived {message[2]}")
+
+    print(f"Sending Client {message[2]}")
+
+    socket_client.send_string((str(message[2])))
+    print("Sent!")
+finally:
+    socket_client.close()
+    context_client.term()
