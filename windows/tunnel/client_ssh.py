6f2d02f4598ee395f9933f1718a2436bf4ef8687
MarcoItalia <italia.marco.sr@gmail.com>
2026-05-21 12:59:32 +0200

    feat(tunnel): clean up; added the alignment of timestamps in every case it's an issue

--- windows/tunnel/client_ssh.py
+++ windows/tunnel/client_ssh.py
@@ -9,13 +9,15 @@ import time
 host = "172.17.69.255"
 username = "marco"
 password = "marco"
-path = "/mnt/c/users/marco/Documenti/Università/III Anno/Tirocinio"
+path = "/mnt/c/users/marco/Documents/Università/III Anno/Tirocinio"
 working_dir = f"{path}/linux_debian/service/"
 path_local = "C:/Users/marco/Documents/Università/III Anno/Tirocinio/prova_copia"
 
+# connect
 client = paramiko.client.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(host, username=username, password=password)
+# open sftp session, it opens automatically a tunnelled conn
 sftp_session = client.open_sftp()
 
 last_timestamp = timestamp_manager.read_timestamp()
@@ -26,28 +28,58 @@ print(f"2 {sftp_session.chdir(path)}")
 print(f"3 {sftp_session.getcwd()}")
 print(f"4 {path_local}")
 
+# check if script run
+running = script_manager.is_script_running(
+    client, "server_test_ssh.py")
 
+time.sleep(3)
+
+_, stdout, _ = client.exec_command("cat /tmp/script.log")
+print(stdout.read().decode())
+
+# read timestamp server and keep the file open because i could align client and server ts
+print("Opening timestamp server")
 try:
-    files = sftp_session.listdir(
-        "/mnt/c/users/marco/Documenti/Università/III Anno/Tirocinio/prova_da_copiare")
+    with sftp_session.open(f"{working_dir}timestamp.json", mode="r") as f:
+        timestamp_server = dtns.dict_to_namespace(json.load(f))
+        print(timestamp_server)
+except Exception as e:
+    print(f"Exception: {e}")
+
+    # if the server ts > client ts check if the data is there to be read.
+if timestamp_server.last_timestamp > last_timestamp:
+    print("checking if the data is there..")
+    files = sftp_session.listdir(f"{path}/prova_da_copiare")
     numbers = [int(f.replace(".txt", "")) for f in files if f.endswith(".txt")]
-    minimo = min(numbers)
-
-    if minimo > last_timestamp:
-        with sftp_session.open(f"{working_dir}timestamp.json", mode="w+", encoding="utf-8") as f:
+    try:
+        minim = min(numbers)
+    except ValueError:
+        minim = timestamp_server.last_timestamp
+    if minim > last_timestamp:
+        print(f"2.1 {minim} > {last_timestamp}")
+        if running:
+            script_manager.script_kill(client, "server_test_ssh.py")
+            running = False
+        with sftp_session.open(f"{working_dir}timestamp.json", mode="w+") as f:
             json.dump(vars(timestamp_manager.json_read()), f, indent=4)
-except ValueError:
-    pass
+elif timestamp_server.last_timestamp < last_timestamp:
+    print(f"2.2 {timestamp_server.last_timestamp} < {last_timestamp}")
+    if running:
+        script_manager.script_kill(client, "server_test_ssh.py")
+        running = False
+    with sftp_session.open(f"{working_dir}timestamp.json", mode="w+") as f:
+        json.dump(vars(timestamp_manager.json_read()), f, indent=4)
 
-if not script_manager.is_script_running(client, "server_test_ssh.py"):
-    print("Starting script..")
-    script_manager.start_script(
-        client, f"{working_dir}server_test_ssh.py")
-    time.sleep(1)
-else:
-    print("Script already running")
+
+if not running:
+    script_manager.start_script(client, f"{working_dir}/server_test_ssh.py")
+
+time.sleep(3)
+_, stdout, _ = client.exec_command("cat /tmp/script.log")
+print(stdout.read().decode())
+
 i = 0
-while i < 10:
+while i < 10:  # while true
     try:
         sftp_session.get(f"{path}/prova_da_copiare/{str(last_timestamp)}.txt",
                          f"{path_local}/{str(last_timestamp)}.txt")
@@ -58,7 +90,6 @@ while i < 10:
             f"{path}/prova_da_copiare/{str(last_timestamp - 1)}.txt")
         time.sleep(1)
     except IOError:
-
         time.sleep(2)
     except Exception as e:
         print(f"Errore: {e}")
