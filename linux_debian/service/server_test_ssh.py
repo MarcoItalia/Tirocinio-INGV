de0bf5bd36db48f01b7b70653eb14678c485f13f
MarcoItalia <italia.marco.sr@gmail.com>
2026-05-21 16:45:58 +0200

    chore(service): added comments to service server

--- linux_debian/service/server_test_ssh.py
+++ linux_debian/service/server_test_ssh.py
@@ -2,12 +2,16 @@ import time
 import timestamp_manager
 
 last_timestamp = timestamp_manager.read_timestamp()
+
 time.sleep(2)
+
 for i in range(10):
     try:
 
+        # gen a document in the local dir
         with open((f"/mnt/c/users/marco/Documenti/Università/III Anno/Tirocinio/prova_da_copiare/{str(last_timestamp)}.txt"), "w+", encoding="utf-8") as f:
             f.write("Stuff " + str(last_timestamp))
+        # check for permissions..
 
         last_timestamp += 1
         timestamp_manager.save_last_timestamp(last_timestamp)
