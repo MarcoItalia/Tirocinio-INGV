1fbfc87a5e3304a406193b06dd2fb5d045c72bdf
MarcoItalia <italia.marco.sr@gmail.com>
2026-06-11 12:24:58 +0200

    feat(service): minor changes to imports in ZMQ_data_collector. Restored timestamp_manager

--- linux_debian/service/ZMQ_data_collector.py
+++ linux_debian/service/ZMQ_data_collector.py
@@ -1,12 +1,11 @@
-# IMPORT
-from timestamp_manager import read_timestamp, save_last_timestamp
-from netcdf4_h5_manager import h5_file_write
+from os import mkdir, walk
+from sys import exit as sys_exit
 from datetime import datetime, timezone
-import numpy as np
+from array import array
+from numpy import reshape, double
+from netcdf4_h5_manager import h5_file_write
+from timestamp_manager import read_timestamp, save_last_timestamp
 import zmq
-import array
-import os
-import sys
 import config_manager
 
 
@@ -17,11 +16,11 @@ PORT = config.socket.port
 IP_ADDRESS = config.socket.ip
 PROTOCOL = config.socket.protocol
 QUEUE_DIM = config.data_dir.queue_dim
-socket_str = f"{PROTOCOL}://{IP_ADDRESS}:{PORT}"
+SOCKET_STR = f"{PROTOCOL}://{IP_ADDRESS}:{PORT}"
 
 # CREATE DIR, if exist ignore the error
 try:
-    os.mkdir(SAVE_PATH)
+    mkdir(SAVE_PATH)
 except FileExistsError:
     pass
 
@@ -29,7 +28,7 @@ except FileExistsError:
 # ZMQ connection
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
-socket.connect(socket_str)
+socket.connect(SOCKET_STR)
 # ZMQ REQ/REP
 socket.send(read_timestamp())
 message1 = socket.recv()
@@ -38,30 +37,30 @@ print(f"Connection started with {IP_ADDRESS}")
 
 # DATA INITIALIZATION
 # Getting information
-data1 = array.array('i', message1[0:4])
+data1 = array('i', message1[0:4])
 COUNT = data1[0]
-data1 = array.array('d', message1[4:])
+data1 = array('d', message1[4:])
 TimeStamp = data1[0]
 print(f"Recived data {datetime.fromtimestamp(TimeStamp, tz=timezone.utc)}")
 if COUNT == 1:
-    data2 = array.array('d', message2[0:48])
+    data2 = array('d', message2[0:48])
     dt = data2[1]
     print(f"dt1: {dt}")
 
-    data2 = array.array('i', message2[48:64])
+    data2 = array('i', message2[48:64])
     Size_Dist = data2[1]-data2[0]
     Size_Frequence = data2[3]-data2[2]
 
     message3 = socket.recv()
-    data3 = array.array('f', message3)
+    data3 = array('f', message3)
 else:
     print("   ")
     print("Script stopped --->>>  Raw data is not a valid dataset")
-    sys.exit()
+    sys_exit()
 # Constructing data
-StrainRate = np.reshape(data3, (Size_Frequence + 1, Size_Dist + 1))
+StrainRate = reshape(data3, (Size_Frequence + 1, Size_Dist + 1))
 # Writing data
-filenames = next(os.walk(SAVE_PATH), (None, None, []))[2]
+filenames = next(walk(SAVE_PATH), (None, None, []))[2]
 if len(filenames) < QUEUE_DIM:
     print("Writing in the Queue")
     h5_file_write(f"{SAVE_PATH}/{str((TimeStamp))}",
@@ -76,36 +75,34 @@ save_last_timestamp(TimeStamp)
 i = 0
 # CONTINOUS COLLECTION
 # controllare che la connessione sia valida se da errore
-while i < 60:  # True
+while True:
 
     print()
-
     # ZMQ REQ/REP
     # reconnect ?
-    socket.send(np.double(TimeStamp))
+    socket.send(double(TimeStamp))
     message1 = socket.recv()
     message2 = socket.recv()
     message3 = socket.recv()
 
     # Getting information
-    data2 = array.array('d', message2[0:48])
+    data2 = array('d', message2[0:48])
     dt = data2[1]
 
-    data2 = array.array('i', message2[48:64])
+    data2 = array('i', message2[48:64])
     Size_Dist = data2[1]-data2[0]  # channels
     Size_Frequence = data2[3]-data2[2]  # frequences
 
-    data1 = array.array('d', message1[4:])
+    data1 = array('d', message1[4:])
     print(f"Recived data {datetime.fromtimestamp(TimeStamp, tz=timezone.utc)}")
 
     if TimeStamp < data1[0]:
         i += 1
         TimeStamp = data1[0]
 
-        data3 = array.array('f', message3)
-        StrainRate = np.reshape(data3, (Size_Frequence + 1, Size_Dist+1))
-        filenames = next(os.walk(SAVE_PATH), (None, None, []))[
-            2]
+        data3 = array('f', message3)
+        StrainRate = reshape(data3, (Size_Frequence + 1, Size_Dist+1))
+        filenames = next(walk(SAVE_PATH), (None, None, []))[2]
         if len(filenames) < QUEUE_DIM:
             print("Writing in the Queue")
             h5_file_write(f"{SAVE_PATH}/{str((TimeStamp))}",
