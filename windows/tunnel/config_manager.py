a3298a0ec093a9251b398ef3399bf41d42c8fcf8
MarcoItalia <italia.marco.sr@gmail.com>
2026-06-11 11:48:47 +0200

    chore(tunnel/service): removed old unused code and aligned service's module

--- windows/tunnel/config_manager.py
+++ windows/tunnel/config_manager.py
@@ -12,19 +12,3 @@ def yaml_read(path_yaml: str = "config.yaml") -> SimpleNamespace:
 
     with open(config_path, encoding="utf-8") as f:
         return dtns.dict_to_namespace(yaml.safe_load(f))
-
-
-def str_constructor(config_namespace: SimpleNamespace = yaml_read(), whois: str = "client") -> str:
-    """Costruisce una stringa'protocollo://ip:porta' leggendo da un SimpleNS"""
-    if whois == "client":
-        return (str(config_namespace.socket.client.protocol) + "://" +
-                str(config_namespace.socket.client.ip) + ":" + str(config_namespace.socket.client.port))
-    elif whois == "server":
-        return (str(config_namespace.socket.server.protocol) +
-                "://" + str(config_namespace.socket.server.ip) + ":" + str(config_namespace.socket.server.port))
-    else:
-        try:
-            return (str(config_namespace.socket.protocol) + "://" +
-                    str(config_namespace.socket.ip) + ":" + str(config_namespace.socket.port))
-        except:
-            return -1
