f4a7489cfbefcbd772234714e69fd6f56b947dfb
MarcoItalia <italia.marco.sr@gmail.com>
2026-06-11 12:35:13 +0200

    chore(service): minor changes in import. Removed useless comments

--- linux_debian/service/timestamp_manager.py
+++ linux_debian/service/timestamp_manager.py
@@ -1,18 +1,18 @@
 from os import path
 from types import SimpleNamespace
-import json
+from json import load, dump
 import numpy as np
 import dict_to_namespace as dtns
 
 
 def __json_read(path_json: str) -> SimpleNamespace:
     """Legge un file json e ritorna un oggetto facilmente leggibile da python"""
-    BASE_DIR = path.dirname(path.abspath(
-        __file__))  # si deve decisamente trasformare in un modulo, refactor needed
-    timestamp_path = path.join(BASE_DIR, path_json)
+    base_dir = path.dirname(path.abspath(
+        __file__))
+    timestamp_path = path.join(base_dir, path_json)
 
     with open(timestamp_path, encoding="utf-8") as f:
-        return dtns.dict_to_namespace(json.load(f))
+        return dtns.dict_to_namespace(load(f))
 
 
 def read_timestamp(path_json: str = "timestamp.json") -> np.double:
@@ -25,12 +25,12 @@ def read_timestamp(path_json: str = "timestamp.json") -> np.double:
 def save_last_timestamp(timestamp, path_json: str = "timestamp.json"):
     """Salva il timestamp nel file"""
 
-    BASE_DIR = path.dirname(path.abspath(
-        __file__))  # si deve decisamente trasformare in un modulo, refactor needed
-    timestamp_path = path.join(BASE_DIR, path_json)
+    base_dir = path.dirname(path.abspath(
+        __file__))
+    timestamp_path = path.join(base_dir, path_json)
 
     json_file = __json_read(path_json)
     json_file.last_timestamp = timestamp
 
     with open(timestamp_path, 'w+', encoding="utf-8") as f:
-        json.dump(vars(json_file), f, indent=4)
+        dump(vars(json_file), f, indent=4)
